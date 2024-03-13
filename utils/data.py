10495f231045418d247914c2d32012be5e79835b
Hanna <hannao2@web.de>
2024-03-13 12:25:36 +0100

    other dataset

--- utils/data.py
+++ utils/data.py
@@ -56,8 +56,8 @@ class iCIFAR100(iData):
     class_order = np.arange(100).tolist()
 
     def download_data(self):
-        train_dataset = datasets.cifar.CIFAR100("./data", train=True, download=True)
-        test_dataset = datasets.cifar.CIFAR100("./data", train=False, download=True)
+        train_dataset = datasets.cifar.CIFAR100("./data", train=True, download=False)
+        test_dataset = datasets.cifar.CIFAR100("./data", train=False, download=False)
         self.train_data, self.train_targets = train_dataset.data, np.array(
             train_dataset.targets
         )
