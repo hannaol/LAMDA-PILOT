c2861e820814ad2ef4511ac6ea949c9562246118
Hanna <hannao2@web.de>
2024-03-13 13:14:01 +0100

    fix

--- main.py
+++ main.py
@@ -24,5 +24,5 @@ def setup_parser():
 
 if __name__ == '__main__':
     os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-    os.environ["CUDA_VISIBLE_DEVICES"]=0
+    os.environ["CUDA_VISIBLE_DEVICES"]="0"
     main()
